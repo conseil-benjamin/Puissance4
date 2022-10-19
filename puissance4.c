90adadb38bc447005ec374dff933a0897a70e231
conseil-benjamin <benlenormand61@gmail.com>
2022-10-19 14:30:58 +0200

    Debut programme

---
+++ puissance4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+showBoard();
+showWinner();
+checkWinner();
+playerMove();
+
+int board;
+int espaceRestant = 42;  /* équivaut au nombre de case vide 
+                           au départ du jeux. */
+
+int main(){
+    printf("Hello world !!");
+}
+
+
+showBoard(){
+   
+}
+
+playerMove(){
+
+}
+
+checkWinner(){
+
+}
+
+
+showWinner(){
+
+}
