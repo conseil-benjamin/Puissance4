3c819a087dc2e613aa1c98c9dc6d5d042220b6e0
Benjamin <benlenormand61@gmail.com>
2022-10-22 01:21:43 +0200

    Modification tableau

--- puissance4.c
+++ puissance4.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-showBoard();   // affiche le tableau de jeux
+char Player1 = 'X';
+char Player2 = 'O';
+
+char board [7][6];
+int nbRangee;
+char winner = ' ';
+int espaceRestant = 42;
+
+void showBoard();   // affiche le tableau de jeux
 showWinner();  // affiche le gagnan (joueur 1 ou 2)
 checkWinner(); // vérifie si un des deux joueurs à gagner 
 playerMove();  // retient en mémoire le mouvement du joueur pour l'afficher sur le tableau
 
-char Player1 = 'X';
-char Player2 = 'O';
-
-char board [7];
-int nbRangee;
-char winner = ' ';
+ /* équivaut au nombre de case vide 
+                                  au départ du jeux. */
 
 
 int main(){
 
-    int espaceRestant = 42;    /* équivaut au nombre de case vide 
-                                  au départ du jeux. */
-   while (espaceRestant != 0 && winner = ' ')
+   while (espaceRestant != 0 && winner == ' ')
    {
     showBoard();
    }
@@ -26,35 +28,29 @@ int main(){
 }
 
 
-showBoard(){
-    printf("    V \n");
-    printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[0], board[0], board[0], board[0], board[0], board[0], board[0]);
-    printf("\n|---|---|---|---|---|---|---|\n");
-    printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[1], board[1], board[1], board[1], board[1], board[1], board[1]);
-    printf("\n|---|---|---|---|---|---|---|\n");
-    printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[2], board[2], board[2], board[2], board[2], board[2], board[2]);
-    printf("\n|---|---|---|---|---|---|---|\n");
-    printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[3], board[3], board[3], board[3], board[3], board[3], board[3]);
-    printf("\n|---|---|---|---|---|---|---|\n");
-    printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[4], board[4], board[4], board[4], board[4], board[4], board[4]);
-    printf("\n|---|---|---|---|---|---|---|\n");
-    printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[5], board[5], board[5], board[5], board[5], board[5], board[5]);
-    printf("\n|---|---|---|---|---|---|---|\n");
-   playerMove();
+void showBoard(){
+     
+
+    for (int i = 0; i < 6; i++)
+    {
+        printf("|%c   | %c  | %c  | %c  | %c  | %c  | %c  |", board[i][0], board[i][1], board[i][2], board[i][3], board[i][4], board[i][5], board[i][6]);
+        printf("\n|---|---|---|---|---|---|---|\n");
+    }
+    playerMove();
 }
 
 playerMove(){
     printf("Joueur 1 : Choisissez une rangée avec le curseur");
     scanf("%d", &nbRangee);
 
-    board [nbRangee] = Player1;
+    board [nbRangee][nbRangee] = Player1;
     espaceRestant --;
 
 
     printf("Joueur 2 : Choisissez une rangée avec le curseur");
     scanf("%d", &nbRangee);
 
-    board[nbRangee] = Player2;        
+    board[nbRangee][nbRangee] = Player2;        
     espaceRestant --;
 
 }
